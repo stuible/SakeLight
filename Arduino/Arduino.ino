80f231df4e2211418ced9de99f3e465e53ffcee6
shrewdlove <stuible@JoshMacBookPro-2.local>
2016-04-07 01:03:13 -0700

    Massive Reorganization

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -15,6 +15,11 @@ int RFIDid;
 PN532_SPI interface(SPI, 10); // create a PN532 SPI interface with the SPI CS terminal located at digital pin 10
 NfcAdapter nfc = NfcAdapter(interface); // create an NFC adapter object
 
+//public Sound
+int delayval = 500; // delay for half a second
+const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
+unsigned int sample;
+unsigned int peakToPeak = 0;   // peak-to-peak level
 
 void setup() {
   Serial.begin(9600);
@@ -27,52 +32,10 @@ void setup() {
 
 void loop() {
 
-  //MOTION
-  val = digitalRead(pirPin); //read state of the PIR
-  
-  if (val == HIGH) { //if the value read is high, there was no motion
-    if (motionLevel > 0) {
-      motionLevel -= 1;
-    }
-    Serial.print("&");
-    Serial.print(motionLevel);
-    Serial.print("&");
-    
-  }
-  else if (val == LOW) { //if the value read was low, there was motion
-    motionLevel += 1;
-    Serial.print("&");
-    Serial.print(motionLevel);
-    Serial.print("&");
-  }
+  getSoundInput();
 
-  
+  checkMotion();
 
-  //VOLUME
-  Serial.print(volumeLevel);
-  Serial.print("&");
-
-
-  //NFC
-  if (nfc.tagPresent()) { // Do an NFC scan to see if an NFC tag is present
-      NfcTag tag = nfc.read(); // read the NFC tag
-      if(tag.hasNdefMessage()) {
-        NdefMessage message = tag.getNdefMessage();
-        for(int i=0;i<message.getRecordCount();i++)
-        {
-          NdefRecord record = message.getRecord(i);
-          int payloadLength = record.getPayloadLength();
-          byte payload[payloadLength];
-          record.getPayload(payload);
-          RFIDid = (payload,payloadLength);
-        }
-      }
-  } else {
-    RFIDid = 0;
-  }
-  Serial.print(RFIDid);
-  Serial.print("&");
-  
-  Serial.println();
+  checkNFC();
   delay(1000);
 }
---
+++ Arduino/Sound.ino
@@ -0,0 +1,30 @@
+void getSoundInput(){
+  long startMillis= millis();  // Start of sample window 
+   unsigned int signalMax = 0;
+   unsigned int signalMin = 1024;
+ 
+   // collect data for 50 mS
+   while (millis() - startMillis < sampleWindow)
+   {
+      sample = analogRead(0);
+      if (sample < 1024)  // toss out spurious readings
+      {
+         if (sample > signalMax)
+         {
+            signalMax = sample;  // save just the max levels
+         }
+         else if (sample < signalMin)
+         {
+            signalMin = sample;  // save just the min levels
+         }
+      }
+   }
+   peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+   double volts = (peakToPeak * 3.3) / 1024;  // convert to volts
+   volumeLevel = peakToPeak;
+
+   
+  //VOLUME
+  Serial.print(volumeLevel);
+  Serial.print("&");
+}
