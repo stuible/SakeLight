b05a658624c45c7b3a2d5f0d000946247406b405
shrewdlove <stuible@JoshMacBookPro-2.local>
2016-04-07 01:42:43 -0700

    Formatting

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,4 +1,4 @@
-//MOTION
+//Motion Variables
 int pirPin = 3; //pin the motion sensor is connected to
 int val; //voltage of the motion sensor pin
 int motionLevel; //current motion level
@@ -6,14 +6,30 @@ int volumeLevel; //current volume level
 int RFIDid; //id of the last rfid tag scanned
 
 
-//NFC
+//NFC libraires
 #include <SPI.h>
 #include "PN532_SPI.h"
 #include "PN532.h"
 #include "NfcAdapter.h"
- 
+
+//Setup NFC interface
 PN532_SPI interface(SPI, 10); // create a PN532 SPI interface with the SPI CS terminal located at digital pin 10
 NfcAdapter nfc = NfcAdapter(interface); // create an NFC adapter object
+
+//Pixel Libraries
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN            6
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      16
+
+// Setup NeoPixel
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 //public Sound
 int delayval = 500; // delay for half a second
